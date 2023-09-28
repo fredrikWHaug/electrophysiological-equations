268dae36de6e899348a6e810b1d202063994f1d4
Fredrik Willumsen Haug <fredrikwhaug@gmail.com>
2023-09-28 06:30:51 -0400

    Comment Add

--- src/utils.py
+++ src/utils.py
@@ -21,6 +21,7 @@ def nernst_potential_calculation(c_out, c_in, z):
     nernst_potential = (60 / z) * np.log10(c_out/ c_in)
     return round(nernst_potential, 2)
 
+# goldman-hodgkin-katz helper function declaration
 def membrane_potential_calculation(p_k, p_na, p_cl, c_kout, 
                                    c_naout, c_clout, c_kin, c_nain, c_clin):
       v_mem = 60 * np.log10( ( (p_k * c_kout) + (p_na * c_naout) + (p_cl * c_clin) ) / ((p_k * c_kin) + (p_na * c_nain) + (p_cl * c_clout)))
